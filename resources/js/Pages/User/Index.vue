a0ee2ccdc66b383e1182d106aa0f505836b7ac49
Anh Khoa <binm8.4356@gmail.com>
2021-11-21 01:57:01 +0700

    fix error for function user

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -14,61 +14,38 @@
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
-                                        <tr>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Tên
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Biệt danh
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Trạng thái
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Chức vụ
-                                            </th>
-                                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                Số hồ sơ
-                                            </th>
-                                            <th scope="col" class="relative px-6 py-3">
-                                                <span class="sr-only">Edit</span>
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="bg-white divide-y divide-gray-200">
-                                        <tr v-for="user in users.data" :key="user.id">
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ user.name }}
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm text-gray-900">{{ user.username }}</div>
-                                                <div class="text-sm text-gray-500">{{ user.email }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                    {{ user.roles[0].name == 'member' ? 'Chưa kích hoạt' : 'Đã kích hoạt' }}
-                                                </span>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                {{ user.roles[0].name.toUpperCase() }}
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                {{ user.customers || user.customers_appraised_count || 0 }}
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <button @click.prevent="modifyRole(user, 'saler')" class="text-indigo-600 hover:text-indigo-900 mr-2">Saler</button>
-                                                <button @click.prevent="modifyRole(user, 'appraiser')" class="text-indigo-600 hover:text-indigo-900 mr-2">Thẩm định</button>
-                                                <button @click.prevent="modifyRole(user, 'disburser')" class="text-indigo-600 hover:text-indigo-900 mr-2">Giải ngân</button>
-                                                <button @click.prevent="modifyRole(user, 'member')" class="text-indigo-600 hover:text-indigo-900 mr-2">Hạ quyền</button>
-                                                <button @click.prevent="destroy(user.id)" class="text-indigo-600 hover:text-indigo-900">Xóa</button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                <Table :headers="['Tên', 'Biệt danh', 'Trạng thái', 'Chức vụ', 'Số hồ sơ']" :model="users">
+                                    <tr v-for="user in users.data" :key="user.id">
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ user.name }}
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm text-gray-900">{{ user.username }}</div>
+                                            <div class="text-sm text-gray-500">{{ user.email }}</div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                {{ user.roles[0].name == 'member' ? 'Chưa kích hoạt' : 'Đã kích hoạt' }}
+                                            </span>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {{ user.roles[0].name.toUpperCase() }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {{ user.customers || user.customers_appraised_count || 0 }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <button @click.prevent="modifyRole(user, 'saler')" class="text-indigo-600 hover:text-indigo-900 mr-2">Saler</button>
+                                            <button @click.prevent="modifyRole(user, 'appraiser')" class="text-indigo-600 hover:text-indigo-900 mr-2">Thẩm định</button>
+                                            <button @click.prevent="modifyRole(user, 'disburser')" class="text-indigo-600 hover:text-indigo-900 mr-2">Giải ngân</button>
+                                            <button @click.prevent="modifyRole(user, 'member')" class="text-indigo-600 hover:text-indigo-900 mr-2">Hạ quyền</button>
+                                            <button @click.prevent="destroy(user.id)" class="text-indigo-600 hover:text-indigo-900">Xóa</button>
+                                        </td>
+                                    </tr>
+                                </Table>
+                                <Pagination :model="users"></Pagination>
                             </div>
                         </div>
                     </div>
@@ -82,12 +59,15 @@
 import AppLayout from '@/Layouts/AppLayout.vue'
 import { Head } from '@inertiajs/inertia-vue3';
 import destroy from '@/Mixins/destroy'
+import Table from '@/Components/Table.vue';
+import Pagination from '@/Components/Pagination.vue';
 
 export default {
     components: {
-        AppLayout,
-        Head,
-    },
+    AppLayout,
+    Head,
+    Table, Pagination
+},
     props: ['users'],
     mixins: [destroy],
     methods: {
